a1843a78617926a83cec2193db577ae970715cb9
JaySeoong <98254816+JaySeoong@users.noreply.github.com>
2025-06-03 17:28:15 +0900

    Update 업그레이드 할 수 없는 아이템 구하기.sql
    주석 추가_250603

--- 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
+++ 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
@@ -4,7 +4,8 @@ WHERE ITEM_ID NOT IN(
     SELECT PARENT_ITEM_ID
     FROM ITEM_TREE
     WHERE PARENT_ITEM_ID IS NOT NULL
-    -- 위, WHERE 조건 없으면 빈 집합을 반환하게 됨
-    -- NOT IN 혹은 IN 내부에 NULL이 있으면 작동하지 않아 NULL 제거가 반드시 선행되어야 함.
+    -- 위, WHERE 조건 없으면 빈 집합을 반환하게 됨 -> 비교를 못함.
+    -- NOT IN 혹은 IN 내부에 NULL이 있으면 동작에 문제가 있을 수 있어, NULL 제거가 반드시 선행되어야 함.
+    -- NOT IN은 전체를 비교하므로 NULL 반드시 제거, IN의 경우에 선일치하는 값이 있을 수 있어 일부 동작.
     )
 ORDER BY ITEM_ID DESC;

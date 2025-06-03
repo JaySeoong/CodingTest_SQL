dfc56843e016889c18c494ea90564ffbd96dbf35
JaySeoong <98254816+JaySeoong@users.noreply.github.com>
2025-06-04 02:48:16 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,13 @@
+SELECT SUM(SCORE) AS SCORE, E.EMP_NO, E.EMP_NAME, E.POSITION, E.EMAIL
+FROM HR_EMPLOYEES AS E
+INNER JOIN HR_GRADE AS G
+ON E.EMP_NO = G.EMP_NO
+WHERE YEAR = 2022 AND POSITION = '팀원'
+GROUP BY E.EMP_NO, E.EMP_NAME, E.POSITION, E.EMAIL
+HAVING SUM(SCORE) = (SELECT MAX(TOTAL_SCORE)
+                    FROM (
+                        SELECT EMP_NO, SUM(SCORE) AS TOTAL_SCORE
+                        FROM HR_GRADE 
+                        WHERE YEAR = 2022 AND POSITION = '팀원'
+                        GROUP BY EMP_NO) AS sub1
+                    );

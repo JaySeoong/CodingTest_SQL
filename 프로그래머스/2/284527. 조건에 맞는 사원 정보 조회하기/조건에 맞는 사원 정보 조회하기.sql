964c70f2d216ef2c3b1bc0af04a3647885100364
JaySeoong <98254816+JaySeoong@users.noreply.github.com>
2025-06-04 02:52:24 +0900

    Update 조건에 맞는 사원 정보 조회하기.sql
    코드수정_250604

--- 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -2,12 +2,12 @@ SELECT SUM(SCORE) AS SCORE, E.EMP_NO, E.EMP_NAME, E.POSITION, E.EMAIL
 FROM HR_EMPLOYEES AS E
 INNER JOIN HR_GRADE AS G
 ON E.EMP_NO = G.EMP_NO
-WHERE YEAR = 2022 AND POSITION = '팀원'
+WHERE YEAR = 2022
 GROUP BY E.EMP_NO, E.EMP_NAME, E.POSITION, E.EMAIL
 HAVING SUM(SCORE) = (SELECT MAX(TOTAL_SCORE)
                     FROM (
                         SELECT EMP_NO, SUM(SCORE) AS TOTAL_SCORE
                         FROM HR_GRADE 
-                        WHERE YEAR = 2022 AND POSITION = '팀원'
+                        WHERE YEAR = 2022
                         GROUP BY EMP_NO) AS sub1
                     );
